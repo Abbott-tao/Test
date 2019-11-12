50380c00e9e44d59333fa5fab01f606bd68cefb9
wangtao <1575077971@qq.com>
2019-11-12 11:22:37 +0800

    第一次提交

---
+++ mavendome/src/main/java/com/gec/entity/User.java
@@ -0,0 +1,11 @@
+package com.gec.entity;
+
+public class User {
+    private String name;
+    private char sex;
+    private int age;
+
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaa");
+    }
+}
